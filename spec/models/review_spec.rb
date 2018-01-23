70da8b0152ee08b0aca32eb9ed800508ec6c2150
lbarstow <lbarstow@macalester.edu>
2018-01-23 14:31:49 -0500

    added explicit tests to make sure that we had errors for the correct attribute

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -19,13 +19,14 @@ RSpec.describe Review, type: :model do
     expect(review.victory_points).to eq(0)
   end
 
-  it "is not valid wihout rating" do
+  it "is not valid without rating" do
     game = Game.new(name: "Lords of Waterdeep", description: "This is a game based on the Dungeons & Dragons universe.")
     user = User.new()
 
     review = Review.new(game: game, user: user)
 
     expect(review).to_not be_valid
+    expect(review.errors[:rating]).to_not be_blank
   end
 
   it "is not valid if rating is not an integer between 1 and 5" do
@@ -37,9 +38,13 @@ RSpec.describe Review, type: :model do
     review_3 = Review.new(rating: 6, game: game, user: user)
 
     expect(review_1).to_not be_valid
+    expect(review_1.errors[:rating]).to_not be_blank
     expect(review_2).to_not be_valid
+    expect(review_2.errors[:rating]).to_not be_blank
     expect(review_3).to_not be_valid
+    expect(review_3.errors[:rating]).to_not be_blank
   end
+
   it "victory points can't be a float" do
     game = Game.new(name: "Lords of Waterdeep", description: "This is a game based on the Dungeons & Dragons universe.")
     user = User.new()
@@ -47,6 +52,7 @@ RSpec.describe Review, type: :model do
     review = Review.new(rating: 2, game: game, user: user, victory_points: 2.5)
 
     expect(review).to_not be_valid
+    expect(review.errors[:victory_points]).to_not be_blank
   end
 
 end
