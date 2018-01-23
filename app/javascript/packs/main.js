db5715f3c300beabafb8ad512950a3831f18c8d6
Jack <jack21222@gmail.com>
2018-01-23 10:55:27 -0500

    First pass at integrating react

---
+++ app/javascript/packs/main.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import jQuery from 'jquery'
+
+jQuery(() => {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+  );
+})
