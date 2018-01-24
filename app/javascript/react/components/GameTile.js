9c962c98baf6959a16101a046cb00ef811c5eda5
lbarstow <lbarstow@macalester.edu>
2018-01-24 13:58:01 -0500

    Removed commented out code from GameTile

--- app/javascript/react/components/GameTile.js
+++ app/javascript/react/components/GameTile.js
@@ -1,14 +1,12 @@
 import React from "react";
-//import { Link } from "react-router"
+
 const GameTile = (props) => {
   return(
-    // <Link path=`/games/${props.id}`>
       <div className = "panel small-3 columns" >
         <h5>{props.name}</h5>
         <h6>Category</h6>
         <p>{props.description}</p>
       </div>
-    //</Link>
   )
 }
 
