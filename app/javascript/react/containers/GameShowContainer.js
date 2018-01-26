f0ca058fe30d16e32703e784c560eee7b1c9d7a4
Jack <jack21222@gmail.com>
2018-01-26 10:03:28 -0500

    Started created game show container and game show tile

---
+++ app/javascript/react/containers/GameShowContainer.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import GameShowTile from '../components/GameShowTile';
+
+class GameShowContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      description: '',
+      categories: [],
+      min_players: null,
+      max_players: null
+    }
+  }
+
+  render() {
+    return(
+
+    )
+  }
+}
+
+export default GameShowContainer;
