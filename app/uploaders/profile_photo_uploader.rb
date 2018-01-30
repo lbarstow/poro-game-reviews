e6200a9befb2817d6566e08a289b4fc60eb7dd38
michalinaj <michalina@jablonska.me>
2018-01-30 14:51:54 -0500

    push before merge of user games relationship

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -1,9 +1,9 @@
 class ProfilePhotoUploader < CarrierWave::Uploader::Base
-  # if Rails.env.test?
-  #   storage :file
-  # else
+  if Rails.env.test?
+    storage :file
+  else
     storage :fog
-  # end
+  end
 #
 # # to be checked later:
   def store_dir
