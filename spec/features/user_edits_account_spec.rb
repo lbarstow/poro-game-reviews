2cae3184a5ba239a8cc3a520571712911dd30751
michalinaj <michalina@jablonska.me>
2018-01-31 12:51:10 -0500

    have edit tests for edit

--- spec/features/user_edits_account_spec.rb
+++ spec/features/user_edits_account_spec.rb
@@ -12,27 +12,68 @@ feature 'user is able to edit his account', %Q{
   # * If I don't specify the required information, I am presented with
   #   an error message
 
-  scenario 'provide valid registration information' do
+  scenario 'provide updated information' do
     visit new_user_registration_path
 
     fill_in 'Email', with: 'john@example.com'
-    fill_in 'Password', with: 'password'
-    fill_in 'Password confirmation', with: 'password'
+    fill_in 'Password', with: 'banana'
+    fill_in 'Password confirmation', with: 'banana'
     fill_in 'Username', with: 'username_1'
-
+    attach_file 'Profile photo', "#{Rails.root}/spec/support/images/photo.jpeg"
 
     click_button 'Sign up'
 
     expect(page).to have_content('Welcome! You have signed up successfully.')
     expect(page).to have_content('Sign Out')
+
+    visit edit_user_registration_path
+
+    expect(page).to have_content('Edit Your Profile')
+    expect(page).to have_content('Sign Out')
+
+    fill_in 'Email', with: 'jane@example.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    fill_in 'Current password', with: 'banana'
+    attach_file 'Profile photo', "#{Rails.root}/spec/support/images/photo2.jpeg"
+
+
+    click_button 'Update'
+    expect(page).to have_css("img[src*='thumb_photo2.jpeg']")
+
   end
 
-  scenario 'provide invalid registration information' do
+  scenario 'user must provide current password to update the account' do
     visit new_user_registration_path
 
+    fill_in 'Email', with: 'john@example.com'
+    fill_in 'Password', with: 'banana'
+    fill_in 'Password confirmation', with: 'banana'
+    fill_in 'Username', with: 'username_1'
+    attach_file 'Profile photo', "#{Rails.root}/spec/support/images/photo.jpeg"
+
     click_button 'Sign up'
-    expect(page).to have_content("can't be blank")
-    expect(page).to_not have_content('Sign Out')
+
+    expect(page).to have_content('Welcome! You have signed up successfully.')
+    expect(page).to have_content('Sign Out')
+
+    visit edit_user_registration_path
+
+
+    expect(page).to have_content('Edit Your Profile')
+    expect(page).to have_content('Sign Out')
+
+    fill_in 'Email', with: 'jane@example.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+
+    attach_file 'Profile photo', "#{Rails.root}/spec/support/images/photo2.jpeg"
+
+
+    click_button 'Update'
+    expect(page).to have_content("Current password can't be blank")
+    expect(page).to have_content('Edit Your Profile')
+
   end
 
 end
