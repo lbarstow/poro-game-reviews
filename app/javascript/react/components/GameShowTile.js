cf08339b5322c864a29117bebc3659856e376b6a
Jack <jack21222@gmail.com>
2018-02-01 17:58:33 -0500

    added average reviews to game show tile and added tests

--- app/javascript/react/components/GameShowTile.js
+++ app/javascript/react/components/GameShowTile.js
@@ -7,6 +7,7 @@ const GameShowTile = (props) => {
       <hr/>
       <h3>Category: {props.categories}</h3>
       <h3>Players: {props.min_players} to {props.max_players} players</h3>
+      <h4>Average Rating: {props.average_rating}</h4>
       <br/>
       <p>{props.description}</p>
     </div>
